95b2358c72d242a532d525fb8df2f036b60940c0
Luis Felipe Cadavid <felipe.cadavid@udea.edu.co>
2023-02-25 01:19:53 -0500

    First Commit

---
+++ menu.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "base_struct.h"
+
+const int main(int argc, char *argv[])
+{
+    const int Menu(char opc, char bandera){
+        
+    }
+}
